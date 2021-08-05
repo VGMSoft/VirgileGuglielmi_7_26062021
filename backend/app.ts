f1881989647d93c63ca7359fb4c56e1b09558509
Virgile Guglielmi <virgile.gug@gmail.com>
2021-08-06 00:12:41 +0200

    :lock: retricting CORS permissions
    retricting CORS permissions

--- backend/app.ts
+++ backend/app.ts
@@ -24,7 +24,7 @@ import helmet = require ("helmet")
 app.use(helmet())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  //res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   next()
