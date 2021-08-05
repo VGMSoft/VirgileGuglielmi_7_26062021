8ccc7e80083ea90c1fdd886613a5bb297d4cf831
Virgile Guglielmi <virgile.gug@gmail.com>
2021-08-06 00:01:58 +0200

    :lock: sanitize users inputs
    sanitize users inputs

---
+++ backend/middleware/sanitize.ts
@@ -0,0 +1,17 @@
+/* This Sanitize module permits
+to remove all the potentially dangerous
+special char or sql keywords of each textual
+data given by the user  */
+
+const regex = /[@#$%&\/?!|*+)(}{=:.>_<"^\[\]]|SELECT|INSERT|ALTER|DELETE|FROM|script/g
+
+const sanitize = (obj) => {
+  Object.keys(obj).forEach(key => {
+    if (typeof obj[key] === "string" && key !== "imageUrl") {
+      obj[key] = obj[key].replace(regex, '')
+    }
+  })
+  return obj
+}
+
+export default sanitize
