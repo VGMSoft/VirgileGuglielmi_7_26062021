2a08d63880e99ba9f593516315ef244206571802
Virgile Guglielmi <virgile.gug@gmail.com>
2021-08-09 14:15:46 +0200

    :lipstick: relocating scss in components
    relocating scss in components

--- frontend/groupomania/src/App.vue
+++ frontend/groupomania/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <Navbar/>
-  <div class="container d-flex align-items-center justify-content-center flex-md-column">
-    <LogBox></LogBox>
-    <Profile></Profile>
+  <Navbar></Navbar>
+
+  <div class="container d-flex justify-content-center pt-5">
+
+<!--    <LogBox></LogBox>
+    <Profile></Profile>-->
     <Post></Post>
-    <p class="text-secondary">{{ numberOfPost }} posts</p>
   </div>
 </template>
 
 <script lang="ts">
-//import * as bootstrap from 'bootstrap';
 import {defineComponent} from 'vue'
 
 export default defineComponent({
   name: 'App',
-
   setup() {
-    return { numberOfPost: 146 };
+    return {
+
+    }
   }
 })
 </script>
 
 <style lang="scss">
-@import './../sass/main';
-@import '~bootstrap/scss/bootstrap';
-//@use '~font-awesome/css/font-awesome.min.css';
+@import '../scss/main';
+
 </style>
