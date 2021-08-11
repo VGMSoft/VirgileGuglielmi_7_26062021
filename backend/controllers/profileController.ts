ce41730a1be0378a98e3da5fda7282a801492c2b
Virgile Guglielmi <virgile.gug@gmail.com>
2021-08-11 11:13:00 +0200

    :lock: add CryptoJS middleware for email encryption
    add CryptoJS middleware for email encryption

--- backend/controllers/profileController.ts
+++ backend/controllers/profileController.ts
@@ -1,14 +1,39 @@
 import {Request, Response} from 'express'
 import User from './../models/TS/userModel'
+import {encrypt, key, iv} from "../middleware/cryptoJS"
+import bcrypt from 'bcrypt'
+
 
 export const createProfile = (req: Request, res: Response, next) => {
-  User.create({...req.body})
-    .then(() => res.status(201).json({message: 'User saved successfully!'}))
-    .catch(error => res.status(400).json({error}))
+  User.findOne({where: {email: encrypt(req.body.email, key, iv)}})
+    .then(email => {
+      if (email) {
+        res.status(405).json({message: 'Account already exists !'})
+      } else {
+        //Password is minimum 8 chars, 1 uppercase, 1 letter, 1 number, 1 special char
+        const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        const pass = req.body.password
+        if (pass.match(passRegex)) {
+          bcrypt.hash(pass, 10)
+            .then(hash => {
+              User.create({
+                ... req.body,
+                email: encrypt(req.body.email, key, iv),
+                password: hash
+              })
+                .then(() => res.status(201).json({message: 'User saved successfully!'}))
+                .catch(error => res.status(401).json({error}))
+            })
+        } else {
+          res.status(404).json({message: 'Le mot de passe fourni ne correspond pas au format demandé !'})
+        }
+      }
+    })
+    .catch(error => res.status(500).json({error}))
 }
 
 export const getProfile = (req: Request, res: Response, next) => {
-  User.findOne({where: {id: req.params.id}})
+  User.findOne({where: {id: req.body.id}})
     .then(user => res.status(200).json(user))
     .catch(error => res.status(404).json({error}))
 }
@@ -28,8 +53,9 @@ export const deleteProfile = (req: Request, res: Response, next) => {
 
 //TODO v
 export const addAvatar = (req: Request, res: Response, next) => {
-  User.update({},{where: {}})
-    .then(() => {})
+  User.update({}, {where: {}})
+    .then(() => {
+    })
     .catch(error => res.status(200).json({error}))
 }
 
