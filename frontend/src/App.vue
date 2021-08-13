d0665d03f365c614e7eb9498c0ac1012eb6eb611
Virgile Guglielmi <virgile.gug@gmail.com>
2021-08-13 21:19:28 +0200

    :truck: delete empty directory

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<Navbar></Navbar>
+
+  <div class="container d-flex justify-content-center pt-5">
+     <LogBox></LogBox>
+       <!--  <Profile></Profile>
+    <Post></Post>-->
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss">
+@import '../scss/main';
+
+</style>
