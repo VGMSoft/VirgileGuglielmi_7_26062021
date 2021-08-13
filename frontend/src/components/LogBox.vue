402282fe4a518fce4b9ca5ef980418c6e55379ec
Virgile Guglielmi <virgile.gug@gmail.com>
2021-08-13 21:19:28 +0200

    :truck: delete empty directory

---
+++ frontend/src/components/LogBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <ul class="nav nav-tabs card-header-tabs">
+        <li class="nav-item">
+          <a class="nav-link " :class="buttonClasses" aria-current="true" href="#">Sign in</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" :class="buttonClasses" href="#">Sign up</a>
+        </li>
+      </ul>
+    </div>
+    <div class="card-body">
+      <SignIn></SignIn>
+      <!--<SignUp></SignUp>-->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue'
+//import SignUp from "./../components/SignUp"
+import SignIn from "./../components/SignIn"
+
+export default defineComponent({
+  name: "LogBox",
+  components: {
+    SignIn,
+    //SignUp
+  },
+  setup() {
+    return {
+      buttonClasses: ref({
+        'active': true,
+      }),
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-dark bg-secondary navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img
+          src="../assets/logos/icon-left-font-monochrome-white.svg"
+          alt="Groupomania Logo"
+        />
+      </a>
+      <button class="navbar-toggler btn rounded-circle overflow-hidden btn-outline-primary border-3 p-0" type="button"
+              data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <img :src="avatarUrl" class="rounded-circle profile-pic" alt="Profile picture">
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item"></li>
+        </ul>
+        <div class="d-flex">
+          <button class="btn btn-outline-primary mx-1 py-0" type="submit">Signup</button>
+          <button class="btn btn-outline-primary mx-1 py-0" type="submit">Signin</button>
+          <button class="btn rounded-circle overflow-hidden btn-outline-primary border-3 p-0" type="submit">
+            <img :src="avatarUrl" class="rounded-circle profile-pic" alt="Profile picture"></button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Navbar",
+  setup() {
+    return {avatarUrl: "https://i.pravatar.cc/150?img=55"}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  &-brand img {
+    width: 10rem;
+  }
+}
+</style>
