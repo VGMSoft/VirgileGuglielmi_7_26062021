fd79c1ed6731056dc9252644e6d19f7c35f308e4
Virgile Guglielmi <virgile.gug@gmail.com>
2021-08-13 18:19:38 +0200

    :lock: passport promise error
    passport promise error

--- backend/middleware/passport.config.ts
+++ backend/middleware/passport.config.ts
@@ -2,13 +2,11 @@
 //TODO https://www.youtube.com/watch?v=-RCnNyD0L-s
 //TODO https://www.youtube.com/watch?v=Ne0tLHm1juE&t=1200s
 
-
 import * as path from 'path'
 import * as fs from 'fs'
 import passport from 'passport'
 import {Strategy, ExtractJwt} from 'passport-jwt'
 import User from '././../models/TS/userModel'
-
 
 const pathToKey = path.join(__dirname, './keyGen', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
@@ -20,23 +18,21 @@ const options = {
 }
 
 export const initPassport = (passport) => {
-  passport.use(new Strategy(options, function(jwt_payload, done) {
+  passport.use(new Strategy(options, function (jwt_payload, done) {
     console.log(jwt_payload)
-    User.findOne({where:{user: jwt_payload.sub}})
+    User.findOne({where: {user: jwt_payload.sub}})
       .then(
-        /*(err, user) => {
-        if (err) {
-          return done(err, false);
+        (user) => {
+          if (user) {
+            return done(null, user)
+          } else {
+            return done(null, false)
+          }
         }
-        if (user) {
-          return done(null, user);
-        } else {
-          return done(null, false);
-          // or you could create a new account
-        }
-      }*/
       )
-      .catch()
+      .catch((err) => {
+        return done(err, false)
+      })
   }))
 }
 
