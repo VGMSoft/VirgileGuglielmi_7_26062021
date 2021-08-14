4ce469a7d4c8144b8e04d3af923ad9c23f6e7619
Virgile Guglielmi <virgile.gug@gmail.com>
2021-08-15 00:36:58 +0200

    :art: views components structure
    views components structure

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+
+  components: {
+
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss">
+@import '../../scss/main';
+</style>
---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss">
+@import '../../scss/main';
+</style>
