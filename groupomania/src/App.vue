edb7f5d404490272162a0f30eb330492fb6fb4f3
Virgile Guglielmi <virgile.gug@gmail.com>
2021-09-03 13:09:04 +0200

    :adhesive_bandage: clean App.vue
    clean App.vue

--- groupomania/src/App.vue
+++ groupomania/src/App.vue
@@ -1,24 +1,12 @@
-<template>
-  <nav class="d-flex">
-    <router-link to="/" class="btn btn-outline-primary m-1">Home</router-link>
-    <router-link to="/auth" class="btn btn-outline-primary m-1">Auth</router-link>
-    <router-link to="/posts" class="btn btn-outline-primary m-1">Posts</router-link>
-    <button @click="showProfile('Catalina')" class="btn btn-outline-primary m-1">Profile</button>
-  </nav>
-  <router-view/>
+<template >
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  name: 'App',
-  setup() {
-    function showProfile (name: string) {
-      this.$router.push({name: 'profile', params: {name: name}})
-    }
-    return {showProfile}
-  }
+  name: 'App'
 })
 </script>
 
