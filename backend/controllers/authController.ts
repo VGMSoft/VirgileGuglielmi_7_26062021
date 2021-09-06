d5f1744a4b4d48474421742ed620c0956fb7e774
Virgile Guglielmi <virgile.gug@gmail.com>
2021-09-06 11:16:36 +0200

    :lock: .then => async await
    .then => async await

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -7,58 +7,66 @@ import {encrypt, iv, key} from "../middleware/cryptoJS"
 import * as path from "path"
 import * as fs from "fs"
 
-export const signup = (req: Request, res: Response, next) => {
-  User.findOne({where: {email: encrypt(req.body.email, key, iv)}})
-    .then(email => {
-      if (email) {
-        res.status(405).json({message: 'Account already exists !'})
-      } else {
+//
+const PRIV_KEY = fs.readFileSync(path.join(__dirname, './../lib/keyGen', 'id_rsa_priv.pem'), 'utf8')
+const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
+export const signup = async (req: Request, res: Response, next) => {
+  try {
+    let email = await User.findOne({where: {email: encrypt(req.body.email, key, iv)}})
+    if (email) {
+      res.status(405).json({message: 'Account already exists !'})
+    } else {
+      try {
         //Password is minimum 8 chars, 1 uppercase, 1 letter, 1 number, 1 special char
-        const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
         const pass = checkpass(req.body.password)
         if (pass.match(passRegex)) {
-          bcrypt.hash(pass, 10)
-            .then(hash => {
-              User.create({
-                ... req.body,
-                email: encrypt(req.body.email, key, iv),
-                password: hash
-              })
-                .then(() => res.status(201).json({message: 'User saved successfully!'}))
-                .catch(error => res.status(401).json({error}))
+          let hash = await bcrypt.hash(pass, 10)
+          try {
+            await User.create({
+              ...req.body,
+              email: encrypt(req.body.email, key, iv),
+              password: hash
             })
-        } else {
-          res.status(404).json({message: 'Le mot de passe fourni ne correspond pas au format demandé !'})
+            res.status(201).json({message: 'User saved successfully!'})
+          } catch (error) {
+            res.status(401).json({error})
+          }
         }
+      } catch {
+        res.status(404).json({message: 'Le mot de passe fourni ne correspond pas au format demandé !'})
       }
-    })
-    .catch(error => res.status(500).json({error}))
+    }
+  } catch (error) {
+    res.status(500).json({error})
+  }
 }
 
-const pathToKey = path.join(__dirname, './../lib/keyGen', 'id_rsa_priv.pem');
-const PRIV_KEY = fs.readFileSync(pathToKey, 'utf8');
 
-export const login = (req, res, next) => {
-  User.findOne({where: {email: encrypt(req.body.email, key, iv)}})
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({error: 'Utilisateur non trouvé !'})
-      }
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({error: 'Mot de passe incorrect !'})
-          }
-          res.status(200).json({
-            userId: user.id,
-            token: jwt.sign(
-              {userId: user.id},
-              `${PRIV_KEY}`,
-              {expiresIn: '24h', algorithm: 'RS256'}
-            )
-          })
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({where: {email: encrypt(req.body.email, key, iv)}})
+    if (!user) {
+      return res.status(401).json({error: 'Utilisateur non trouvé !'})
+    }
+    try {
+      const valid = await bcrypt.compare(req.body.password, user.password)
+      if (!valid) {
+        return res.status(401).json({error: 'Mot de passe incorrect !'})
+      } else {
+        res.status(200).json({
+          userId: user.id,
+          token: jwt.sign(
+            {userId: user.id},
+            `${PRIV_KEY}`,
+            {expiresIn: '24h', algorithm: 'RS256'}
+          )
         })
-        .catch(error => res.status(500).json({error}))
-    })
-    .catch(error => res.status(500).json({error}))
+      }
+    } catch (error) {
+      res.status(500).json({error})
+    }
+  } catch (error) {
+    res.status(500).json({error})
+  }
 }
