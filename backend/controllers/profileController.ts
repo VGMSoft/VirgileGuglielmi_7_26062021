bba950154a2ff911db2f0d7dd80fdd0c5edb16c9
Virgile Guglielmi <virgile.gug@gmail.com>
2021-10-03 01:25:53 +0200

    :lipstick: user friendly req.data
    user friendly req.data

--- backend/controllers/profileController.ts
+++ backend/controllers/profileController.ts
@@ -1,12 +1,17 @@
 import {Request, Response} from 'express'
 import User from './../models/TS/userModel'
+import {decrypt, iv, key} from "../middleware/cryptoJS";
 
 //OK
 export const getProfile = async (req, res: Response, next) => {
   const userId: string = req.user.dataValues.id
   try {
-    const user = await User.findOne({where: {id: userId}})
-    res.status(200).json(user)
+    let user = await User.findOne({where: {id: userId}})
+    res.status(200).json({
+      ...req.user.dataValues,
+      email: decrypt(req.user.dataValues.email, key, iv),
+      signup_date: JSON.stringify(req.user.dataValues.signup_date).slice(1,11)
+    })
   } catch (error) {
     res.status(404).json({error})
   }
@@ -31,7 +36,7 @@ export const editProfile = async (req, res: Response, next) => {
 
 export const deleteProfile = async (req, res: Response, next) => {
   const userId: string = req.params.id
-    // req.user.dataValues.id
+  // req.user.dataValues.id
   try {
     await User.destroy({where: {id: userId}})
     res.status(201).json({message: 'User deleted successfully!'})
