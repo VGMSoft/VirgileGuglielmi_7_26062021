fde43e878317944e2495f610ba795e004b0b1dd4
Virgile Guglielmi <virgile.gug@gmail.com>
2021-10-05 03:46:37 +0200

    :sparkles: edit and suppr fx
    edit and suppr fx

--- frontend/src/components/PostCompo.vue
+++ frontend/src/components/PostCompo.vue
@@ -1,121 +1,170 @@
-<template class="d-flex flex-column">
-  <div class="d-flex flex-column justify-content-center align-items-center welcome h-100 my-5">
-    <div class="d-flex flex-column">
-      <ul class="list-unstyled">
-        <li v-for="post in posts" :key="post">
-          <div class="card post shadow-sm mb-5">
-            <div class="card-header d-flex flex-row justify-content-between bg-light bg-gradient p-1">
-              <div class="d-flex flex-row align-items-center">
-                <img src="@/assets/profile/avatar.png" class="rounded-circle profile-pic border border-primary border-1"
-                     alt="avatar"/>
-                <h5 class="card-title text-red mx-2 mb-0">{{ post.user.pseudo }}</h5>
-                <!--              <p class="card-text text-end text-muted"><small>{{ post.time }}</small></p>-->
-              </div>
-              <div class="dropdown">
-                <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <font-awesome-icon :icon="['fas', 'ellipsis-h']" class="me-1"/>
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <router-link to="/posts/:id/edit" class="dropdown-item mb-0 p-1">
-                    <font-awesome-icon :icon="['fas', 'pen']" class="text-secondary me-1"/>
-                    Editer ...
-                  </router-link>
-                  <div class="dropdown-item mb-0 p-1">
-                    <font-awesome-icon :icon="['fas', 'trash']" class="text-secondary me-1" @click="deletePost()"/>
-                    Supprimer ...
+<template>
+  <main class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="dark-text fw-bold">fil d'actualité</h1>
+        <NewPostCompo/>
+        <ul class="list-unstyled" v-if="posts">
+          <li v-for="post in posts" :key="post">
+            <div class="card post shadow-sm mb-3">
+              <div class="card-header d-flex flex-row justify-content-between bg-light bg-gradient p-1">
+                <div class="d-flex flex-row align-items-center">
+                  <img src="@/assets/profile/avatar.png"
+                       class="rounded-circle profile-pic border border-primary border-1"
+                       alt="avatar"/>
+                  <h5 class="card-title text-red mx-2 mb-0">{{ post.user.pseudo }}</h5>
+                  <!--              <p class="card-text text-end text-muted"><small>{{ post.time }}</small></p>-->
+                </div>
+                <div v-if="post.user.id == userId || isAdmin()" class="dropdown">
+                  <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton"
+                          data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <font-awesome-icon :icon="['fas', 'ellipsis-h']" class="me-1"/>
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <div class="dropdown-item mb-0 p-1" @click="toggleEdit()">
+                      <font-awesome-icon :icon="['fas', 'pen']" class="text-secondary me-1"/>
+                      <span v-if="edit">Undo Edit</span>
+                      <span v-else>Edit Profile</span>
+                    </div>
+                    <div class="dropdown-item mb-0 p-1" @click="deletePost(post.id)">
+                      <font-awesome-icon :icon="['fas', 'trash']" class="text-secondary me-1"/>
+                      Supprimer
+                    </div>
                   </div>
                 </div>
               </div>
+              <div class="card-body text-secondary border-bottom-0">
+                <p class="card-text ">{{ post.content }}</p>
+                <input v-if="edit && post.user.id == userId || isAdmin()" type="text"
+                       class="form-control border border-warning"
+                       placeholder="New content text"
+                       v-model.trim="postContent"/>
+
+                <button v-if="edit && post.user.id == userId || isAdmin()" type="submit"
+                        class="btn btn-outline-warning rounded-pill m-2"
+                        @click="editPost(post.id)">
+                  <font-awesome-icon :icon="['fas', 'pen']" class="text-blue"/>
+                  Edit
+                </button>
+                <button v-if="edit && post.user.id == userId || isAdmin()" type="submit"
+                        class="btn btn-outline-warning rounded-pill m-2" @click="toggleEdit()">
+                  <font-awesome-icon :icon="['fas', 'times']" class="text-red"/>
+                  Exit
+                </button>
+              </div>
+              <div class="card-footer d-flex justify-content-between border-top-0 py-0">{{ post.date }}
+
+              </div>
             </div>
-            <div class="card-body text-secondary border-bottom-0">
-              <p class="card-text ">{{ post.content }}</p>
-              <p class="card-text ">{{ post.id }}</p>
-            </div>
-<!--            <div class="card-footer d-flex justify-content-between border-top-0 py-0">-->
-<!--              <button class="btn btn-outline-primary rounded-pill btn-sm">-->
-<!--                <font-awesome-icon :icon="['fas', 'thumbs-up']" class="me-1"/>-->
-<!--                Like-->
-<!--              </button>-->
-<!--              <button class="btn btn-outline-primary rounded-pill btn-sm">-->
-<!--                <font-awesome-icon :icon="['fas', 'comment']" class="me-1"/>-->
-<!--                Comment-->
-<!--              </button>-->
-<!--            </div>-->
-          </div>
-        </li>
-      </ul>
+          </li>
+        </ul>
+        <div v-else>
+          <Spinner/>
+        </div>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
 import {defineComponent, onMounted, ref} from 'vue'
 import {http} from "@/config/axios.config"
 import {PostModel} from "@/models/postModel"
+import Cookies from "js-cookie";
+import NewPostCompo from "@/components/NewPostCompo.vue";
+import Spinner from "@/components/SpinnerCompo.vue";
 
 export default defineComponent({
   name: "Post",
-
+  components: {NewPostCompo, Spinner},
   setup() {
+
+    // edit mode initial state
+    let edit = ref(false)
+
+    const toggleEdit = () => {
+      console.log(edit.value)
+      if (edit.value === false) {
+        edit.value = true
+      } else if (edit.value === true) {
+        edit.value = false
+      }
+      return edit.value
+    }
+
+    // check user identity
+    const userId = Cookies.get('userId');
+    const isAdmin = () => {
+      return Cookies.get('userRole') === 'ADMIN';
+    }
+
     let posts = ref<Array<PostModel> | null>(null);
+
+    let postContent = ref("")
+
+
     onMounted(async () => {
       const axiosResponse = await http.get<Array<PostModel>>('/api/posts')
       posts.value = axiosResponse.data
-      console.log(axiosResponse.data)
-
     })
-    const createPost = async () => {
+
+    // const getOnePost = (id: number) => {
+    //   http.get<PostModel>(`/api/posts/${id}`)
+    //     .then((response) => {
+    //       return response.data
+    //     })
+    // }
+
+
+    const getPosts = async () => {
       try {
-        const response = await http.post<PostModel>(`/api/posts`)
+        const axiosResponse = await http.get<Array<PostModel>>('/api/posts')
+        posts.value = axiosResponse.data
+      } catch (err) {
+        return err
+      }
+    }
+
+    const editPost = async (id: number) => {
+      try {
+        const response = await http.put<PostModel>(`/api/posts/${id}`, postContent)
+        edit.value = false
+        getPosts()
         return response.data
       } catch (err) {
         return err
       }
     }
 
-    // const getAllPosts = async () => {
-    //   try {
-    //     const posts = await http.get<Array<PostModel>>('/api/posts')
-    //     console.log(posts.data)
-    //     return posts.data
-    //   } catch (err) {
-    //     return err
-    //   }
-    // }
-
-    const getOnePost = (id: number) => {
-      http.get<PostModel>(`/api/posts/${id}`)
-        .then((response) => {
-          return response.data
-        })
+    const deletePost = async (id: number) => {
+      try {
+        const response = await http.delete<PostModel>(`/api/posts/${id}`)
+        getPosts()
+        return response.data
+      } catch (err) {
+        return err
+      }
     }
 
-    const editPost = (id: number) => {
-      http.put<PostModel>(`/api/posts/${id}`)
-        .then((response) => {
-          return response.data
-        })
-    }
 
-    const deletePost = (id: number) => {
-      http.delete<PostModel>(`/api/posts/${id}`)
-        .then((response) => {
-          return response.data
-        })
-    }
-
-    return {posts, getOnePost, createPost, editPost, deletePost}
+    return {userId, isAdmin, posts, edit, postContent, toggleEdit, editPost, deletePost}
   }
 })
 </script>
 
 <style lang="scss" scoped>
-.post {
-  width: 40rem;
-
-  &-send {
-    height: 100%;
+.dark-text {
+  color: #091E43;
+  font-variant: small-caps;
+  &::after {
+    content: "";
+    display: block;
+    background-color: #D15059;
+    width: 100%;
+    height: 3px;
+    margin-top: 0.2em;
+    margin-bottom: 0.5em;
   }
 }
+
 </style>
--- frontend/src/components/ProfileCompo.vue
+++ frontend/src/components/ProfileCompo.vue
@@ -1,49 +1,105 @@
 <template>
-  <div class="card " v-if="user">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src="@/assets/profile/avatar.png" class="img-fluid rounded-start" alt="Profile picture">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <div class="d-flex justify-content-between">
-            <h5 class="card-title text-red fw-bold">{{ user.pseudo }}</h5>
-            <div class="dropdown">
-              <button class="btn btn-secondary btn-sm dropdown-toggle text-end" type="button" id="dropdownMenuButton"
-                      data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <font-awesome-icon :icon="['fas', 'cog']" class="me-1"/>
-              </button>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <div class="dropdown-item mb-0 p-1" @click="editProfile()">
-                  <font-awesome-icon :icon="['fas', 'pen']" class="me-1"/>
-                  Edit Profile
-                </div>
-                <div class="dropdown-item mb-0 p-1">
-                  <font-awesome-icon :icon="['fas', 'lock']" class="me-1"/>
-                  Changer mot de passe
-                </div>
-                <div class="dropdown-item mb-0 p-1" @click="deleteProfile()">
-                  <font-awesome-icon :icon="['fas', 'trash']" class="me-1"/>
-                  Supprimmer le compte
+  <div class="d-flex justify-content-center mt-3">
+    <div class="d-flex flex-column">
+      <h1 class="dark-text fw-bold">profile</h1>
+      <div class="card " v-if="user">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="@/assets/profile/avatar.png" class="img-fluid rounded-start" alt="Profile picture">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <div class="d-flex justify-content-between">
+                <h5 class="card-title text-red fw-bold">{{ user.pseudo }}</h5>
+
+                <!--Dropdown-->
+                <div class="dropdown mb-2">
+                  <button class="btn btn-secondary btn-sm dropdown-toggle text-end" type="button"
+                          id="dropdownMenuButton"
+                          data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <font-awesome-icon :icon="['fas', 'cog']" class="me-1"/>
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <div class="dropdown-item mb-0 p-1" @click="toggleEdit()">
+                      <font-awesome-icon :icon="['fas', 'pen']" class="me-1"/>
+                      <span v-if="edit">Undo Edit</span>
+                      <span v-else>Edit Profile</span>
+                    </div>
+                    <div class="dropdown-item mb-0 p-1">
+                      <font-awesome-icon :icon="['fas', 'lock']" class="me-1"/>
+                      Changer mot de passe
+                    </div>
+                    <div class="dropdown-item mb-0 p-1" @click="deleteProfile()">
+                      <font-awesome-icon :icon="['fas', 'trash']" class="me-1"/>
+                      Supprimer le compte
+                    </div>
+                  </div>
                 </div>
               </div>
+
+              <!--Pseudo-->
+              <input v-if="edit" type="text"
+                     class="form-control border border-warning mb-3"
+                     placeholder="Nouveau pseudo"
+                     required
+                     v-model.trim="editValues.pseudo"/>
+
+              <p class="card-text mb-3"><strong>Type de compte : </strong>{{ user.role }}</p>
+
+              <!--Names-->
+              <p class="card-text"><strong>Nom complet : </strong>{{ user.firstname }} {{ user.lastname }}</p>
+              <div class="row mb-3">
+                <div class="col">
+                  <input v-if="edit" type="text"
+                         class="form-control border border-warning"
+                         placeholder="Nouveau prénom"
+                         required
+                         v-model.trim="editValues.firstname"/>
+                </div>
+                <div class="col">
+                  <input v-if="edit" type="text"
+                         class="form-control border border-warning"
+                         placeholder="Nouveau nom"
+                         required
+                         v-model.trim="editValues.lastname"/>
+                </div>
+              </div>
+
+              <!--Mail-->
+              <p class="card-text"><strong>Email : </strong>{{ user.email }}</p>
+
+              <input v-if="edit" type="email"
+                     class="form-control border border-warning mb-3"
+                     placeholder="Nouvel email"
+                     required
+                     v-model.trim="editValues.email">
+
+              <p class="card-text">Inscrit depuis le {{ user.signup_date }}</p>
+              <div>
+                <label for="formFile" class="form-label"><strong class="text-muted">Upload un avatar ?</strong></label>
+                <input class="form-control form-control" id="formFile" type="file" disabled>
+              </div>
+              <button v-if="edit" type="submit" class="btn btn-outline-warning rounded-pill m-2" @click="editProfile()">
+                <font-awesome-icon :icon="['fas', 'pen']" class="text-blue"/>
+                Edit
+              </button>
+              <button v-if="edit" type="submit" class="btn btn-outline-warning rounded-pill m-2" @click="toggleEdit()">
+                <font-awesome-icon :icon="['fas', 'times']" class="text-red"/>
+                Exit
+              </button>
             </div>
           </div>
-          <p class="card-text"><strong>Type de compte :</strong>{{ user.role }}</p>
-          <p class="card-text"><strong>Nom complet : </strong>{{ user.firstname }} {{ user.lastname }}</p>
-          <p class="card-text"><strong>Mail : </strong>{{ user.email }}</p>
-          <p class="card-text"><small class="text-muted">Inscrit depuis le {{ user.signup_date }}</small></p>
         </div>
       </div>
+      <div v-else>
+        <Spinner/>
+      </div>
     </div>
-  </div>
-  <div v-else>
-    <Spinner/>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref} from 'vue'
+import {defineComponent, onMounted, reactive, ref} from 'vue'
 import {http} from "@/config/axios.config"
 import {UserModel} from "@/models/userModel"
 import Cookies from "js-cookie";
@@ -57,30 +113,47 @@ export default defineComponent({
   setup() {
     let user = ref<UserModel | null>(null);
     const userId = Cookies.get('userId')
-
-    onMounted(async () => {
-      const axiosResponse = await http.get<UserModel>(`/api/profile/${userId}`);
-      user.value = axiosResponse.data
-
-      const email = axiosResponse.data.email
+    let edit = ref(false)
+    const editValues = reactive({
+      pseudo: "",
+      email: "",
+      firstname: "",
+      lastname: ""
     })
-
 
     const router = useRouter()
 
-    // const getProfile = async (id: number) => {
-    //   try {
-    //     const response = await http.get<UserModel>(`/api/profile/${id}`)
-    //     return response.data
-    //   } catch (err) {
-    //     return err
-    //   }
-    // }
+    onMounted(async () => {
+      edit.value = false
+      const axiosResponse = await http.get<UserModel>(`/api/profile/${userId}`);
+      user.value = axiosResponse.data
+    })
+
+    const getProfile = async () => {
+      try {
+        const axiosResponse = await http.get<UserModel>(`/api/profile/${userId}`);
+        user.value = axiosResponse.data
+        return axiosResponse.data
+      } catch (err) {
+        return err
+      }
+    }
+
+    const toggleEdit = () => {
+      if (edit.value === false) {
+        edit.value = true
+      } else if (edit.value === true) {
+        edit.value = false
+      }
+      return edit.value
+    }
 
     const editProfile = async () => {
       try {
-        const response = await http.put<UserModel>(`/api/profile/${userId}`)
-        return response.data
+        const editReq = await http.put<UserModel>(`/api/profile/${userId}`, {...editValues})
+        edit.value = false
+        getProfile()
+        return editReq.data
       } catch (err) {
         return err
       }
@@ -89,17 +162,42 @@ export default defineComponent({
     const deleteProfile = async () => {
       try {
         const response = await http.delete<UserModel>(`/api/profile/${userId}`)
+        Cookies.remove('userToken')
+        Cookies.remove('userId')
+        Cookies.remove('userRole')
         await router.push({name: 'home'})
         return response.data
       } catch (err) {
         return err
       }
     }
-    return {user, editProfile, deleteProfile}
+    return {user, editProfile, deleteProfile, edit, toggleEdit, editValues}
   }
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
+.card {
+  width: 80vw;
+}
+
+.dark-text {
+  font-variant: small-caps;
+  color: #091E43;
+  //background-color: #AEAEB0;
+  &::after {
+    content: "";
+    display: block;
+    background-color: #D15059;
+    width: 100%;
+    height: 3px;
+    margin-top: 0.2em;
+    margin-bottom: 0.5em;
+  }
+}
+
+.profile-pic {
+  border-radius: 50%;
+}
 </style>
