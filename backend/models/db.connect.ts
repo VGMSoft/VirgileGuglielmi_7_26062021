cc912fb985fb285bb58d8268f5e5863df011a0fe
Virgile Guglielmi <virgile.gug@gmail.com>
2021-10-08 11:01:40 +0200

    :art: clean code && set Admin
    clean code && set Admin

--- backend/models/db.connect.ts
+++ backend/models/db.connect.ts
@@ -1,7 +1,5 @@
 import {Sequelize} from 'sequelize'
 
-// const sequelize: Sequelize = new Sequelize(`${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
-//TODO : Unable to create  database from scratch
-const sequelize: Sequelize = new Sequelize("mysql://root:Lucas2021!@localhost:3306/groupomania")
+const sequelize: Sequelize = new Sequelize(`${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
 
 export default sequelize
--- backend/models/index.ts
+++ backend/models/index.ts
@@ -1,3 +1,4 @@
+import * as mysql from 'mysql2'
 import sequelize from "./db.connect"
 import {initUser} from "./SQL/userModel.sql"
 import {initPost} from "./SQL/postModel.sql"
@@ -8,6 +9,15 @@ import {initLike} from "./SQL/likeModel.sql"
 import User from "./TS/userModel";
 import {encrypt, iv, key} from "../middleware/cryptoJS";
 import bcrypt from 'bcrypt'
+
+export const createDB = async () => {
+  mysql.createConnection({
+    host: `${process.env.DB_HOST}`,
+    user: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASS}`,
+    database: `${process.env.DB_NAME}`
+  }).query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}; USE ${process.env.DB_NAME};`);
+}
 
 export const connectDB = async () => {
   sequelize.authenticate()
@@ -29,26 +39,26 @@ export const connectDB = async () => {
 }
 
 export const initDB = async () => {
-  initUser()
-  initPost()
-  initComment()
-  initLike()
+  await initUser()
+  await initPost()
+  await initComment()
+  await initLike()
 }
 
+//TODO: only once
 export const setAdmin = async () => {
   try {
-  await User.create({
-    email: encrypt(`${process.env.ADMIN_EMAIL}`, key, iv),
-    password: await bcrypt.hash(`${process.env.ADMIN_PASS}`, 10),
-    firstname: "Admin",
-    lastname: "groupomania",
-    pseudo: "Admin",
-    avatar_url: undefined,
-    signup_date: new Date(),
-    role: "ADMIN"
-  })}
-catch (error)
-  {
+    await User.create({
+      email: encrypt(`${process.env.ADMIN_EMAIL}`, key, iv),
+      password: await bcrypt.hash(`${process.env.ADMIN_PASS}`, 10),
+      firstname: "Admin",
+      lastname: "Groupomania",
+      pseudo: "Admin",
+      avatar_url: undefined,
+      signup_date: new Date(),
+      role: "ADMIN"
+    })
+  } catch (error) {
     return error
   }
 }
