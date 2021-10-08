8021f686cd372b047c47ee36e86e223c3c0bf7f7
Virgile Guglielmi <virgile.gug@gmail.com>
2021-10-08 11:05:56 +0200

    :art: multer import ES6

--- backend/routes/profileRoute.ts
+++ backend/routes/profileRoute.ts
@@ -1,7 +1,7 @@
 import * as express from 'express'
 import {Router} from 'express'
 import * as userCtrl from '../controllers/profileController'
-// import multer from "../middleware/multer.config"
+import multer from "../middleware/multer.config"
 import passport from 'passport'
 
 const profileRoute: Router = express.Router()
