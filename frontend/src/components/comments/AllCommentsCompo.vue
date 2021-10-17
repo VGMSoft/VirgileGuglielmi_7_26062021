b3549edbb32f3b420a6466c5d08c1a3113bd378f
Virgile Guglielmi <virgile.gug@gmail.com>
2021-10-17 23:24:45 +0200

    :lipstick: styling and extract compo
    styling and extract compo

--- frontend/src/components/comments/AllCommentsCompo.vue
+++ frontend/src/components/comments/AllCommentsCompo.vue
@@ -1,49 +1,49 @@
 <template>
-  <main class="container">
+  <main class="container px-1">
     <div class="row">
       <div class="col">
-        <h1 class="text-dark--lighter fw-bold text-uppercase after mt-2">fil d'actualité</h1>
-        <NewPostCompo @onCreatePost="getPosts()"/>
-        <ul class="list-unstyled" v-if="posts">
-          <li v-for="post in posts" :key="post">
-            <!--            <slot></slot>-->
-            <div class="card post shadow-sm mb-3" @click="showPost(post.id)">
-              <div class="card-header d-flex flex-row justify-content-between bg-light bg-gradient p-1">
-                <div class="d-flex flex-row align-items-center">
-                  <img src="@/assets/profile/avatar.png"
-                       class="rounded-circle profile-pic border border-primary border-1"
-                       alt="avatar"/>
-                  <h5 class="card-title text-red mx-2 mb-0">{{ post.user.pseudo }}</h5>
+        <NewCommentCompo @onCreateComment="getComments()"/>
+        <ul class="list-unstyled">
+          <li v-for="comment in comments" :key="comment">
+
+             <div class="card mb-3 shadow-sm">
+              <div class="card-body bg-light p-2 ">
+                <div class=" d-flex flex-row justify-content-between mb-2">
+                  <div class="d-flex flex-row align-items-center">
+                    <img src="@/assets/profile/avatar.png"
+                         class="rounded-circle profile-pic border border-primary border-1"
+                         alt="avatar"/>
+                    <h5 class="card-title text-red mx-2 mb-0">{{ comment.user.pseudo }}</h5>
+                  </div>
+                </div>
+                <div class="ps-2">
+                  <p class="card-text">{{ comment.content }}</p>
                 </div>
               </div>
-              <div class="card-body text-secondary border-bottom-0">
-                <p class="card-text ">{{ post.content }}</p>
-              </div>
-              <div class="card-footer d-flex justify-content-between border-top-0 py-0">{{ post.date }}
-              </div>
+              <div class="card-footer d-flex justify-content-between py-0">{{ comment.date }}</div>
             </div>
+
           </li>
         </ul>
-        <div v-else>
-          <Spinner/>
-        </div>
+        <!--        <div v-else>-->
+        <!--          <Spinner/>-->
+        <!--        </div>-->
       </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref} from 'vue'
+import {defineComponent, inject, onMounted, ref} from 'vue'
 import {http} from "@/config/axios.config"
-import {PostModel} from "@/models/postModel"
 import Cookies from "js-cookie";
-import NewPostCompo from "@/components/posts/NewPostCompo.vue";
-import Spinner from "@/components/utility/SpinnerCompo.vue";
 import {useRouter} from "vue-router";
+import {CommentModel} from '@/models/commentModel';
+import NewCommentCompo from "@/components/comments/NewCommentCompo.vue";
 
 export default defineComponent({
   name: "AllPostCompo",
-  components: {NewPostCompo, Spinner},
+  components: {NewCommentCompo},
   setup() {
     const router = useRouter()
 
@@ -65,32 +65,31 @@ export default defineComponent({
     const isAdmin = () => {
       return Cookies.get('userRole') === 'ADMIN';
     }
-
-    let posts = ref<Array<PostModel> | null>();
+    const postId = inject('postId');
+    let comments = ref<Array<CommentModel> | undefined>();
 
     onMounted(async () => {
-      const axiosResponse = await http.get<Array<PostModel>>('/api/posts')
-      posts.value = axiosResponse.data
+      await getComments()
     })
 
-    const showPost = async (id) => {
-      await router.push(`/posts/${id}`)
-    }
 
-    const getPosts = async () => {
+    const getComments = async () => {
       try {
-        const axiosResponse = await http.get<Array<PostModel>>('/api/posts')
-        posts.value = axiosResponse.data
+        const axiosResponse = await http.get<Array<CommentModel>>(`/api/posts/${postId}/comments`)
+        comments.value = axiosResponse.data
+
+        console.log(comments.value)
+
         return axiosResponse.data
       } catch (err) {
         return err
       }
     }
 
-    const deletePost = async (id: number) => {
+    const deleteComment = async (postId: number) => {
       try {
-        const response = await http.delete<PostModel>(`/api/posts/${id}`)
-        await getPosts()
+        const response = await http.delete<CommentModel>(`/api/posts/${postId}/comments`)
+        await getComments()
         return response.data
       } catch (err) {
         return err
@@ -98,9 +97,9 @@ export default defineComponent({
     }
 
     return {
-      userId, isAdmin, posts,
+      userId, isAdmin, comments,
       edit, toggleEdit,
-      showPost, getPosts, deletePost
+      getComments, deleteComment
     }
   }
 })
