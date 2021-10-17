2c4275eee918b4fe68cf4aa64852e0e856587763
Virgile Guglielmi <virgile.gug@gmail.com>
2021-10-17 23:28:48 +0200

    :sparkles: ready to use comment compo
    ready to use comment compo

--- frontend/src/components/comments/OneCommentCompo.vue
+++ frontend/src/components/comments/OneCommentCompo.vue
@@ -1,13 +1,54 @@
 <template>
-$END$
+  <div class="card mb-3 shadow-sm">
+    <div class="card-body bg-light p-2 ">
+      <div class=" d-flex flex-row justify-content-between mb-2">
+        <div class="d-flex flex-row align-items-center">
+          <img src="@/assets/profile/avatar.png"
+               class="rounded-circle profile-pic border border-primary border-1"
+               alt="avatar"/>
+          <h5 class="card-title text-red mx-2 mb-0">{{ pseudo }}</h5>
+        </div>
+      </div>
+      <div class="ps-2">
+        <p class="card-text">{{ content }}</p>
+      </div>
+    </div>
+    <div class="card-footer d-flex justify-content-between py-0">{{ date }}</div>
+  </div>
 </template>
 
-<script>
-export default {
-name: "OneCommentCompo"
-}
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+
+export default defineComponent({
+  name: "OneCommentCompo",
+  props: {
+    pseudo: {
+      type: String,
+      required: true
+    }, content: {
+      type: String,
+      required: true
+    }, date: {
+      type: String,
+      required: true
+    },
+  }
+
+})
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../../../scss/main';
 
+.card-text::after{
+  content: "";
+  display: block;
+  background-color: #6c757d;
+  width: 100%;
+  height: 1px;
+  margin-top: 1em;
+  margin-bottom: 0.5em;
+}
 </style>
