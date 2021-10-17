8a495d33ca39c025c47e833e02a9f7e0de634163
Virgile Guglielmi <virgile.gug@gmail.com>
2021-10-17 23:10:51 +0200

    :art: modify request content
    modify request content

--- backend/controllers/profileController.ts
+++ backend/controllers/profileController.ts
@@ -6,7 +6,7 @@ import {decrypt, encrypt, iv, key} from "../middleware/cryptoJS";
 export const getProfile = async (req, res: Response, next) => {
   const userId: string = req.user.dataValues.id
   try {
-    let user = await User.findOne({where: {id: userId}})
+    await User.findOne({where: {id: userId}})
     res.status(200).json({
       ...req.user.dataValues,
       email: decrypt(req.user.dataValues.email, key, iv),
@@ -20,24 +20,21 @@ export const getProfile = async (req, res: Response, next) => {
 // OK
 export const editProfile = async (req, res: Response, next) => {
   const userId: string = req.user.dataValues.id
+  //prevent non unique email (too strict)
+  //TODO modify condition
 
   // let email = await User.findOne({where: {email: encrypt(req.body.email, key, iv)}})
   // if (email) {
   //   res.status(405).json({message: 'This email is already used !'})
   // } else {
-    const reqBody = req.body.email
+    const reqBody = req.file
       ? {
         ...req.body,
-        email: encrypt(req.body.email, key, iv)
+        email: encrypt(req.body.email, key, iv),
+        avatar_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
       }
-      : {...req.body}
-    console.log(req)
-//     const reqBody = req.file
-//       ? {
-//         ...req.body,
-//         avatar_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-//       }
-//       : {...req.body}
+      : {email: encrypt(req.body.email, key, iv),
+      ...req.body}
 
     try {
       await User.update({...reqBody, id: userId}, {where: {id: userId}})
@@ -54,26 +51,6 @@ export const deleteProfile = async (req, res: Response, next) => {
   try {
     await User.destroy({where: {id: userId}})
     res.status(201).json({message: 'User deleted successfully!'})
-  } catch (error) {
-    res.status(400).json({error})
-  }
-}
-
-export const addAvatar = async (req, res: Response, next) => {
-  const userId: string = req.user.dataValues.id
-  const profilePic = req.file
-    ? {
-      ...req.body,
-      avatar_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    }
-    : {
-      ...req.body
-    }
-
-  try {
-    console.log(profilePic, req.file)
-    await User.update({...profilePic, id: userId}, {where: {id: userId}})
-    res.status(201).json({message: 'Profile picture added successfully!'})
   } catch (error) {
     res.status(400).json({error})
   }
