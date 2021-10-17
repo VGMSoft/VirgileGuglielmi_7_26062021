ff85e884a908420de1ae8b60581ec83c90eba5f8
Virgile Guglielmi <virgile.gug@gmail.com>
2021-10-17 23:17:37 +0200

    :art: reformat and prapare to upload
    reformat and prapare to upload

--- frontend/src/components/ProfileCompo.vue
+++ frontend/src/components/ProfileCompo.vue
@@ -3,10 +3,15 @@
     <div class="row">
       <div class="col">
         <h1 class="dark-text fw-bold text-uppercase after mt-2">profile</h1>
-        <div class="card " v-if="profile">
+        <div class="card" v-if="profile">
           <div class="row g-0">
             <div class="col-md-4">
-              <img src="@/assets/profile/avatar.png" class="img-fluid rounded-start" alt="Profile picture">
+              <img src="@/assets/profile/avatar.png"
+                   class="img-fluid rounded-start"
+                   alt="Profile picture"/>
+<!--              <img :src=profile.avatar_url-->
+<!--                   class="img-fluid rounded-start"-->
+<!--                   alt="Profile picture"/>-->
             </div>
             <div class="col-md-8">
               <div class="card-body">
@@ -15,25 +20,33 @@
 
                   <!--Dropdown-->
                   <div class="dropdown mb-2">
-                    <button class="btn btn-secondary btn-sm dropdown-toggle text-end" type="button"
+                    <button class="btn btn-secondary btn-sm dropdown-toggle text-end"
+                            type="button"
                             id="dropdownMenuButton"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            data-bs-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false">
                       <font-awesome-icon :icon="['fas', 'cog']" class="me-1"/>
                     </button>
+
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
                       <div class="dropdown-item mb-0 p-1" @click="toggleEdit()">
                         <font-awesome-icon :icon="['fas', 'pen']" class="me-1"/>
                         <span v-if="editState">Undo Edit</span>
                         <span v-else>Edit Profile</span>
                       </div>
+
                       <div class="dropdown-item mb-0 p-1 text-muted">
                         <font-awesome-icon :icon="['fas', 'lock']" class="me-1"/>
                         Changer mot de passe
                       </div>
+
                       <div class="dropdown-item mb-0 p-1" @click="deleteProfile()">
                         <font-awesome-icon :icon="['fas', 'trash']" class="me-1"/>
                         Supprimer le compte
                       </div>
+
                     </div>
                   </div>
                 </div>
@@ -44,14 +57,19 @@
                        placeholder="Nouveau pseudo"
                        required
                        v-model.trim="profile.pseudo"/>
-<!--                       -->
 
-
-                <p class="card-text mb-3"><strong class="text-primary">Type de compte : </strong>{{ profile.role }}</p>
+                <p class="card-text mb-3">
+                  <strong class="text-primary">Type de compte : </strong>
+                  {{ profile.role }}
+                </p>
 
                 <!--Names-->
-                <p class="card-text"><strong class="text-primary">Nom complet : </strong>{{ profile.firstname }}
-                  {{ profile.lastname }}</p>
+                <p class="card-text">
+                  <strong class="text-primary">Nom complet : </strong>
+                  {{ profile.firstname }}
+                  {{ profile.lastname }}
+                </p>
+
                 <div class="row mb-3">
                   <div class="col">
                     <input v-if="editState" type="text"
@@ -59,7 +77,6 @@
                            placeholder="Nouveau prénom"
                            required
                            v-model.trim="profile.firstname"/>
-<!--                           -->
                   </div>
                   <div class="col">
                     <input v-if="editState" type="text"
@@ -67,32 +84,52 @@
                            placeholder="Nouveau nom"
                            required
                            v-model.trim="profile.lastname"/>
-<!--                           -->
                   </div>
                 </div>
 
                 <!--Mail-->
-                <p class="card-text"><strong class="text-primary">Email : </strong>{{ profile.email }}</p>
+                <p class="card-text">
+                  <strong class="text-primary">Email : </strong>
+                  {{ profile.email }}
+                </p>
 
-                <input v-if="editState" type="email"
+                <input v-if="editState"
+                       type="email"
                        class="form-control border border-info mb-3"
                        placeholder="Nouvel email"
                        required
-                       v-model.trim="profile.email"
-                       />
+                       v-model.trim="profile.email"/>
 
-                <p class="card-text"><strong class="text-primary">Inscrit depuis : </strong>{{ profile.signup_date }}</p>
+                <p class="card-text">
+                  <strong class="text-primary">Inscrit depuis : </strong>
+                  {{ profile.signup_date }}
+                </p>
 
-                <button v-if="editState" type="submit" class="btn btn-outline-info rounded-pill m-2"
+                <!--File upload-->
+                <div class="d-flex flex-row mb-3">
+                  <input class="form-control" type="file" @change="onFileSelected"
+                         ref="fileInput"/>
+                  <!--                  <button class="btn btn-outline-info mx-2" @click="$refs.fileInput.click()">Pick file</button>-->
+                  <button class="btn btn-info mx-2" @click="onUpload">Upload</button>
+                </div>
+
+                <!--Edit buttons-->
+                <button v-if="editState"
+                        type="submit"
+                        class="btn btn-outline-info rounded-pill m-2"
                         @click="editProfile()">
                   <font-awesome-icon :icon="['fas', 'pen']" class="text-blue"/>
                   Edit
                 </button>
-                <button v-if="editState" type="submit" class="btn btn-outline-info rounded-pill m-2"
+
+                <button v-if="editState"
+                        type="submit"
+                        class="btn btn-outline-info rounded-pill m-2"
                         @click="toggleEdit()">
                   <font-awesome-icon :icon="['fas', 'times']" class="text-primary"/>
                   Exit
                 </button>
+
               </div>
             </div>
           </div>
@@ -130,17 +167,17 @@ export default defineComponent({
     //router utilities
     const router = useRouter()
 
-    //Models
-    let user = ref<UserModel | undefined>();
-    let profile = ref<UserModel | undefined>();
-
-
 
     onMounted(async () => {
       // editState.value = false
       await getProfile()
     })
 
+    //Models
+    let user = ref<UserModel | undefined>();
+    let profile = ref<UserModel | undefined>();
+
+    //Profile
     const getProfile = async () => {
       try {
         const getReq = await http.get<UserModel>(`/api/profile/${userId}`);
@@ -175,7 +212,35 @@ export default defineComponent({
       }
     }
 
-    return {user, profile, editProfile, editState, deleteProfile, toggleEdit,}
+    //Avatar
+    let selectedFile = undefined
+
+    const onFileSelected = (event) => {
+      console.log("1) event", event)
+      selectedFile = event.target.files[0]
+      console.log("2) selectedFile", selectedFile, selectedFile.name)
+    }
+
+    const onUpload = async () => {
+      const fd = new FormData();
+      fd.append('image', selectedFile, selectedFile.name)
+      console.log("3) formData", fd)
+      const res = await http.put(`/api/profile/2`, fd, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        onUploadProgress: progressEvent => {
+          console.log("upload progress : " + Math.round(progressEvent.loaded / progressEvent.total * 100) + "%")
+        }
+      })
+      console.log("4) res", res)
+    }
+
+    return {
+      user, profile, editState,
+      editProfile, deleteProfile, toggleEdit,
+      onFileSelected, onUpload
+    }
   }
 })
 </script>
